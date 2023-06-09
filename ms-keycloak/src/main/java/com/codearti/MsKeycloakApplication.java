048cfd015cc3c19aa906a727d4ff268a74369120
PieroCahuanaPalomino <piero.cahuana.69.palomino@gmail.com>
2023-06-09 00:03:59 -0500

    Servicio keycloak integrado con Eureka

--- ms-keycloak/src/main/java/com/codearti/MsKeycloakApplication.java
+++ ms-keycloak/src/main/java/com/codearti/MsKeycloakApplication.java
@@ -2,8 +2,10 @@ package com.codearti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsKeycloakApplication {
 
 	public static void main(String[] args) {
